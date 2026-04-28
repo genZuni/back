c853432387ce1760ea324801472413001e0ca6cd
Taha Rahimy <rahimytaha9@gmail.com>
2026-04-28 20:30:33 +0330

    update

--- src/config/winston.config.ts
+++ src/config/winston.config.ts
@@ -1,14 +1,14 @@
-import { format, transports } from 'winston';
+// import { format, transports } from 'winston';
 
 export const winstonConfig = {
-  format: format.combine(
-    format.timestamp(),
-    format.json(),
-    format.prettyPrint(),
-  ),
-  transports: [
-    new transports.Console(),
-    new transports.File({ filename: 'logs/error.log', level: 'error' }),
-    new transports.File({ filename: 'logs/combined.log' }),
-  ],
+  // format: format.combine(
+  //   format.timestamp(),
+  //   format.json(),
+  //   format.prettyPrint(),
+  // ),
+  // transports: [
+  //   new transports.Console(),
+  //   new transports.File({ filename: 'logs/error.log', level: 'error' }),
+  //   new transports.File({ filename: 'logs/combined.log' }),
+  // ],
 };
