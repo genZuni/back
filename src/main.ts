330fde45360a542cae0c4a27929b9af574e250b1
Taha Rahimy <rahimytaha9@gmail.com>
2026-04-28 20:23:44 +0330

    .

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { setupSwagger } from './config/swagger.config';
 import { WinstonModule } from 'nest-winston';
-import { winstonConfig } from './config/winston.config';
+// import { winstonConfig } from './config/winston.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
